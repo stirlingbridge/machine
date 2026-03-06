c6f8a30b56b6c5797cfb7b5c8b35f7c2b1cb8673
David Boreham <david@bozemanpass.com>
2026-03-06 13:10:46 -0700

    Remaining updates for UV

--- sh/dev-setup.sh
+++ sh/dev-setup.sh
@@ -1,5 +1,1 @@
-python3 -m venv dev
-source ./dev/bin/activate
-python3 -m pip install --upgrade pip setuptools wheel
-pip install shiv
-pip install --editable .
+uv sync
--- sh/lint.sh
+++ sh/lint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [[ "$1" == "--fix" ]]; then
-  LINE_LENGTH=$(cat tox.ini | grep 'max-line-length' | cut -d'=' -f2 | awk '{ print $1 }')
-  black -l ${LINE_LENGTH:-132} machine/ 
+  LINE_LENGTH=$(grep 'max-line-length' .flake8 | cut -d'=' -f2 | awk '{ print $1 }')
+  uv run black -l ${LINE_LENGTH:-132} machine/
 fi
 
-flake8 --config tox.ini
+uv run flake8
